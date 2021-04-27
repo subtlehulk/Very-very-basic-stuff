ac9f64234541e247dacec5c59c9aa38cc39cc856
subtlehulk <coreygraham@live.co.uk>
2021-04-27 10:53:23 +0100

    Added OldCode.txt file

--- Program.cs
+++ Program.cs
@@ -4,84 +4,15 @@ namespace csharp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)  //This is a method called "Main" and it is called when the program starts so everything must be in here that we wish to be ran when executed
         {
-
-            /*
-            //Verifying input ==> someone's name
-            Console.WriteLine("Please enter your name.");
-            var userName = Console.ReadLine();
-            Console.WriteLine($"Your name is {userName}, is that correct?");
-            var verificationOfName = Console.ReadLine();
-
-            
-            Basic if statement
-            if (verificationOfName == "yes" || verificationOfName == "Yes" || verificationOfName == "y")
-            {
-                Console.WriteLine("Thank you");
-            } 
-            else
-                Console.WriteLine("Don't lie to me");
-            
-
-            //Using a while loop to verifiy someone's name
-            while (verificationOfName == "No" || verificationOfName == "no" || verificationOfName == "n")
-            {
-                Console.WriteLine("Please enter your correct name");
-                var name = Console.ReadLine();
-                Console.WriteLine("Is this name correct?");
-                var correct = Console.ReadLine();
-                if (correct == "Yes" || correct == "yes" || correct == "y") 
-                {
-                    break;
-                }
-            }
-            Console.WriteLine("Thank you");
-            
-            string nameOne = "";
-            string nameTwo = "";
-            string nameThree = "";
-            
-
-            for (int i=2;i<=3; i++) {
-                Console.WriteLine("What is your name?");
-
-            }
-            
-
-            //Converting Strings into number values.
-            //NOTE: Use decimals/doubles for ease of use. Will convert ints into decimals but will look like a whole integer.
-
-            var number = "30090";
-            var numberConverted = Convert.ToInt32(number);
-            Console.WriteLine(numberConverted);
-
-            var numberTwo = "30090.5";
-            var numberConvertedTwo = Convert.ToDouble(numberTwo);
-            Console.WriteLine(numberConvertedTwo);
-
-            var numberThree = "30090.54568568";
-            var numberConvertedThree = Convert.ToDecimal(numberThree);
-            Console.WriteLine(numberConvertedThree);
-            
-
-            var apples = 100;
-            var oranges = 2;
-            var result = apples % oranges;
-
-            Console.WriteLine(result);
-            
-            result = oranges % apples;
-            
-            Console.Write(result + "\n");
-           
-            Console.WriteLine(DateTime.Now);
-            */
 
             Console.WriteLine("Hello there, Player! What class would you like to be?");
             var userInput = Console.ReadLine();
             var newDeck = new Deck();
 
+            //For future builds, when verifying user input for deck choice, convert input to int and depending on the input, create
+            //a specific class for that build. I.E. 2 = create new Knight();
             if (userInput == "1")
             {
               newDeck.typeOfDeck = "Druid";
@@ -94,6 +25,7 @@ namespace csharp
                 newDeck.typeOfDeck = "Priest";
             }
 
+            
 
             Console.WriteLine("Please name your deck.");
             newDeck.nameOfDeck = Console.ReadLine();
